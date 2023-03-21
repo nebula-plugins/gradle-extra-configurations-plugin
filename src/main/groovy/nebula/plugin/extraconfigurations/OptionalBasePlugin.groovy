e8764dbc20b5f93ab9916741a58fae9640439ad0
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2023-03-21 12:58:22 -0700

    Make OptionalBasePlugin no-op

--- src/main/groovy/nebula/plugin/extraconfigurations/OptionalBasePlugin.groovy
+++ src/main/groovy/nebula/plugin/extraconfigurations/OptionalBasePlugin.groovy
@@ -17,95 +17,14 @@ package nebula.plugin.extraconfigurations
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.publish.PublishingExtension
-import org.gradle.api.publish.ivy.IvyPublication
-import org.gradle.api.publish.maven.MavenPublication
-import org.gradle.api.publish.plugins.PublishingPlugin
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class OptionalBasePlugin implements Plugin<Project> {
-    static final String OPTIONAL_IDENTIFIER = 'optional'
+    static final Logger LOGGER = LoggerFactory.getLogger(ProvidedBasePlugin)
 
     @Override
     void apply(Project project) {
-        enhanceProjectModel(project)
-        configureMavenPublishPlugin(project)
-        configureIvyPublishPlugin(project)
-    }
-
-    /**
-     * Enhances the Project domain object by adding
-     *
-     * a) a extra property List that holds optional dependencies
-     * b) a extra method that can be executed as parameter when declaring dependencies
-     *
-     * @param project Project
-     */
-    private void enhanceProjectModel(Project project) {
-        project.ext.optionalDeps = []
-
-        project.ext.optional = { dep ->
-            project.ext.optionalDeps << dep
-        }
-    }
-
-    /**
-     * Configures Maven Publishing plugin to ensure that published dependencies receive the optional element.
-     *
-     * @param project Project
-     */
-    private void configureMavenPublishPlugin(Project project) {
-        project.plugins.withType(PublishingPlugin) {
-            project.publishing {
-                publications {
-                    project.extensions.findByType(PublishingExtension)?.publications?.withType(MavenPublication) { MavenPublication pub ->
-                        pub.pom.withXml {
-                            project.ext.optionalDeps.each { dep ->
-                                def foundDep = asNode().dependencies.dependency.find {
-                                    it.groupId.text() == dep.group && it.artifactId.text() == dep.name
-                                }
-
-                                if (foundDep) {
-                                    if (foundDep.optional) {
-                                        foundDep.optional.value = 'true'
-                                    } else {
-                                        foundDep.appendNode(OPTIONAL_IDENTIFIER, 'true')
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    /**
-     * Configures Ivy Publishing plugin to ensure that published dependencies receive the correct conf attribute value.
-     *
-     * @param project Project
-     */
-    private void configureIvyPublishPlugin(Project project) {
-        project.plugins.withType(PublishingPlugin) {
-            project.publishing {
-                publications {
-                    project.extensions.findByType(PublishingExtension)?.publications?.withType(IvyPublication) { IvyPublication pub ->
-                        pub.descriptor.withXml {
-                            def rootNode = asNode()
-
-                            // Add optional configuration if it doesn't exist yet
-                            if (!rootNode.configurations.find { it.@name == OPTIONAL_IDENTIFIER }) {
-                                rootNode.configurations[0].appendNode('conf', [name: OPTIONAL_IDENTIFIER, visibility: 'public'])
-                            }
-
-                            // Replace dependency "runtime->default" conf attribute value with "optional"
-                            project.ext.optionalDeps.each { dep ->
-                                def foundDep = rootNode.dependencies.dependency.find { it.@name == dep.name }
-                                foundDep?.@conf = OPTIONAL_IDENTIFIER
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        LOGGER.warn("nebula.optional-base plugin does nothing. Please remove the plugin")
     }
 }
